feb345b383bf1815bf282044df718b6cc365e365
Abhinandana <abhinandu8589@gmail.com>
2025-10-10 02:36:54 +0530

    Complete request ambulance

--- backend/resqway/urls.py
+++ backend/resqway/urls.py
@@ -1,39 +1,23 @@
-"""
-URL configuration for resqway project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from core.views import UserViewSet, IncidentViewSet, HospitalViewSet, AmbulanceViewSet
+from core.views import UserViewSet, IncidentViewSet, HospitalViewSet, AmbulanceViewSet, request_ambulance
 
+# DRF router for viewsets
 router = DefaultRouter()
-router.register('users', UserViewSet)
-router.register('incidents', IncidentViewSet)
-router.register('hospitals', HospitalViewSet)
-router.register('ambulances', AmbulanceViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'incidents', IncidentViewSet)
+router.register(r'hospitals', HospitalViewSet)
+router.register(r'ambulances', AmbulanceViewSet)
 
 urlpatterns = [
+    # API endpoints
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/request-ambulance/', request_ambulance, name='request_ambulance'),
+
+    # Admin site
+    path('admin/', admin.site.urls),
 ]
